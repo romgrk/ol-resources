24fb70d87abd6fd0480852b0c3d61cbcc137a916
Romain Grégoire <gregoirer@ca.objectiflune.com>
2017-02-06 18:11:30 -0500

    array: put everything in a function

--- array.js
+++ array.js
@@ -6,44 +6,123 @@
  */
 
 
-
 /*
- * Array polyfill
+ * Array
  */
 
-Array.prototype.forEach = function(callback) {
-  for (var i = 0; i < this.length; i++)
-    callback(this[i], i, this);
-}
-
-Array.prototype.map = function(callback) {
-  var newArray = []
-  for (var i = 0; i < this.length; i++)
-    newArray[i] = callback(this[i])
-  return newArray;
-}
-
-Array.prototype.reduce = function(callback, accumulator) {
-  for (var i = 0; i < this.length; i++)
-    accumulator = callback(accumulator, this[i])
-  return accumulator;
-}
-
-Array.prototype.filter = function(predicate) {
-  var newArray = []
-  for (var i =0; i < this.length; i++) {
-    if (predicate(this[i]))
-      newArray.push(this[i])
+function extendArray() {
+  Array.from = function(array) {
+    try {
+      return Array.prototype.slice.call(array, 0)
+    } catch(e) {
+      return map(array, function(v) { return v })
+    }
   }
-  return newArray;
-}
 
-Array.prototype.contains = function(value) {
-  for (var i = 0; i < this.length; i++) {
-    if (this[i] == value)
-      return true
+  function forEach(array, callback) {
+    for (var i = 0; i < array.length; i++)
+      callback(array[i], i, array);
   }
-  return false
+
+  function map(array, callback) {
+    var newArray = []
+    for (var i = 0; i < array.length; i++)
+      newArray[i] = callback(array[i], i, array)
+    return newArray;
+  }
+
+  function reduce(array, callback, accumulator) {
+    var i       = accumulator != undefined ? 0 : 1
+    accumulator = accumulator != undefined ? accumulator : array[0]
+    for (; i < array.length; i++)
+      accumulator = callback(accumulator, array[i])
+    return accumulator;
+  }
+
+  function filter(array, predicate) {
+    var newArray = []
+    for (var i =0; i < array.length; i++) {
+      if (predicate(array[i], i, array))
+        newArray.push(array[i])
+    }
+    return newArray;
+  }
+
+  function some(array, predicate) {
+    for (var i = 0; i < array.length; i++) {
+      if (predicate(array[i], i, array))
+        return true
+    }
+    return false
+  }
+
+  function every(array, predicate) {
+    for (var i = 0; i < array.length; i++) {
+      if (!predicate(array[i], i, array))
+        return false
+    }
+    return true
+  }
+
+  function contains(array, value) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] === value)
+        return true
+    }
+    return false
+  }
+
+  function find(array, predicate) {
+    for (var i = 0; i < array.length; i++) {
+      if (predicate(array[i]))
+        return array[i]
+    }
+  }
+
+  function last(array) {
+    return array[array.length - 1]
+  }
+
+  function flatten(array) {
+    return reduce(array, function(acc, cur){ return acc.concat(cur); }, [])
+  }
+
+  function zip(a, b) {
+    return map(a, function(e, i) { return [e, b[i]] })
+  }
+
+  function append(array, other) {
+    Array.prototype.push.apply(array, other)
+    return array
+  }
+
+  function prepend(array, other) {
+    Array.prototype.unshift.apply(array, other)
+    return array
+  }
+
+
+  var functions = {
+    map: map,
+    reduce: reduce,
+    filter: filter,
+    forEach: forEach,
+    some: some,
+    every: every,
+    flatten: flatten,
+    contains: contains,
+    find: find,
+    append: append,
+    prepend: prepend,
+    last: last
+  }
+  for (var n in functions) {
+    (function(name, fn) {
+      Array.prototype[name] = function() {
+        var args = Array.prototype.slice.call(arguments, 0)
+        args.unshift(this)
+        return fn.apply(this, args)
+      }
+    }(n, functions[n]))
+  }
 }
-
-
