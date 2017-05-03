7ba829f1f04a6f2b64dd682a7d301dde0b23fc59
Rom Grk <romgrk.cc@gmail.com>
2017-05-03 11:27:56 -0400

    jscript: remove array functions + comment sendEmail()

--- jscript.js
+++ jscript.js
@@ -18,117 +18,8 @@ function toString(value) { try { return JSON.stringify(value) } catch(e) { retur
 
 
 /*
- * Array
+ * Array: see ./array.js
  */
-
-function forEach(array, callback) {
-  for (var i = 0; i < array.length; i++)
-    callback(array[i], i, array);
-}
-
-function map(array, callback) {
-  var newArray = []
-  for (var i = 0; i < array.length; i++)
-    newArray[i] = callback(array[i], i, array)
-  return newArray;
-}
-
-function reduce(array, callback, accumulator) {
-  var i       = accumulator != undefined ? 0 : 1
-  accumulator = accumulator != undefined ? accumulator : array[0]
-  for (; i < array.length; i++)
-    accumulator = callback(accumulator, array[i])
-  return accumulator;
-}
-
-function filter(array, predicate) {
-  var newArray = []
-  for (var i =0; i < array.length; i++) {
-    if (predicate(array[i], i, array))
-      newArray.push(array[i])
-  }
-  return newArray;
-}
-
-function some(array, predicate) {
-  for (var i = 0; i < array.length; i++) {
-    if (predicate(array[i], i, array))
-      return true
-  }
-  return false
-}
-
-function every(array, predicate) {
-  for (var i = 0; i < array.length; i++) {
-    if (!predicate(array[i], i, array))
-      return false
-  }
-  return true
-}
-
-function contains(array, value) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === value)
-      return true
-  }
-  return false
-}
-
-function last(array) {
-  return array[array.length - 1]
-}
-
-function flatten(array) {
-  return reduce(array, function(acc, cur){ return acc.concat(cur); }, [])
-}
-
-function zip(a, b) {
-  return map(a, function(e, i) { return [e, b[i]] })
-}
-
-function append(array, other) {
-  Array.prototype.push.apply(array, other)
-  return array
-}
-
-function prepend(array, other) {
-  Array.prototype.unshift.apply(array, other)
-  return array
-}
-
-function extendArray() {
-  Array.from = function(array) {
-    try {
-      return Array.prototype.slice.call(array, 0)
-    } catch(e) {
-      return map(array, function(v) { return v })
-    }
-  }
-
-  var functions = {
-    map: map,
-    reduce: reduce,
-    filter: filter,
-    forEach: forEach,
-    some: some,
-    every: every,
-    flatten: flatten,
-    contains: contains,
-    append: append,
-    prepend: prepend,
-    last: last
-  }
-  for (var n in functions) {
-    (function(name, fn) {
-      Array.prototype[name] = function() {
-        var args = Array.prototype.slice.call(arguments, 0)
-        args.unshift(this)
-        return fn.apply(this, args)
-      }
-    }(n, functions[n]))
-  }
-}
-
 
 
 /*
@@ -621,20 +512,22 @@ function httpPOST(url, data) {
 
 
 /** Sends an email.
- * Usage:
+ * @param options {Object} message options
+ * @example
 
   var message = {
-    server:   'smtp.office365.com',
-    port:     25, // Don't include the port if you are targeting offiche365
-    username: 'docrequest@lordco.com',
-    password: 'secret',
-    usessl:   true,
-    headers:  { 'X-Custom': 'value' }
-    from:     'docrequest@lordco.com',
-    to:       'gregoirer@ca.objectiflune.com',
-    attachments: ['filename.txt'],
-    subject:  'Statement',
-    body:     '<b>Yo</b><br>This is a new statement'
+    server:         'smtp.office365.com',
+    port:           25, // Don't include the port if you are targeting office365
+    username:       'docrequest@lordco.com',
+    password:       'secret',
+    usessl:         true, // Set to true if you are targeting office365
+    headers:        { 'X-Custom': 'value' }
+    from:           'docrequest@lordco.com',
+    to:             'gregoirer@ca.objectiflune.com',
+    isHighPriority: true,
+    attachments:    ['c:/absolute/path/to/filename.txt'],
+    subject:        'Statement',
+    body:           '<b>Yo</b><br>This is a new statement'
   }
 
   sendEmail(message);
@@ -665,7 +558,8 @@ function sendEmail(options) {
   message.Subject       = options.subject;
   message.HTMLBody      = options.body;
 
-  if (options.priority == 2) {
+  if (options.isHighPriority) {
+    /* 0 = Low, 1 = Normal, 2 = High */
     message.Fields.Item('urn:schemas:mailheader:X-MSMail-Priority') = 'High'
     message.Fields.Item('urn:schemas:mailheader:X-Priority') = 2
     message.Fields.Item('urn:schemas:httpmail:importance') = 2
